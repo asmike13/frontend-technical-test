153c778929c4a8920c6b62aeb71629814e88f4d1
Mike <assoune.mike@gmail.com>
2022-05-25 17:52:10 +0200

    add conversations apis

---
+++ src/api/conversationsApi.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const baseUrl = process.env.REACT_APP_BASE_URL_API;
+
+export interface Conversation {
+	id: number
+	senderId: number
+	senderNickname: string
+	recipientId: number
+	recipientNickname: string
+	lastMessageTimestamp: number
+}
+
+export const getConversationByUserId = (payload: { userId: number }) => {
+	return axios.get(`${baseUrl}/conversations/${payload.userId}`);
+}
+
+export const postConversationByUserId = (payload: { conversation: Conversation, userId: number }) => {
+	return axios.post(`${baseUrl}/conversations/${payload.userId}`, payload.conversation);
+}
+
+export const deleteConversationById = (payload: { conversationId: number }) => {
+	return axios.delete(`${baseUrl}/conversations/${payload.conversationId}`);
+}
