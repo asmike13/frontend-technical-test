f1914467e3df8e0f9ba6152c7d8bac10576f938d
Mike <assoune.mike@gmail.com>
2022-05-26 05:18:58 +0200

    add avatar component

---
+++ src/components/commons/Avatar.tsx
@@ -0,0 +1,16 @@
+import { Avatar as MuiAvatar } from '@mui/material';
+import { stringToColour } from '../../helpers';
+
+interface IAvatar {
+	str: string
+}
+
+const Avatar = ({ str }: IAvatar) => {
+	return (
+		<MuiAvatar sx={{ bgcolor: stringToColour(str) }}>
+			{str.substring(0, 2).toUpperCase()}
+		</MuiAvatar>
+	)
+}
+
+export default Avatar;
